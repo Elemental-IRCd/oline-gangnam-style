aeadfbd4ae1f915291328f040cda54f309743024
Sam Dodrill <shadow.h511@gmail.com>
2014-04-07 15:33:47 -0700

    Add main application code

---
+++ oline-gangnam-style.py
@@ -0,0 +1,20 @@
+from jinja2 import Environment, FileSystemLoader
+
+import json
+import os
+import sys
+
+env = Environment(loader=FileSystemLoader("."))
+template = env.get_template('ircd.conf.jinja')
+
+config = {}
+
+with open(sys.argv[1] if len(sys.argv) > 1 else "config.json", "r") as fin:
+    config = json.loads(fin.read())
+
+network = config["network"]
+
+for server in config["servers"]:
+    with open("confs/" + server["name"]+".conf", "w") as fout:
+        fout.write(template.render(**locals()))
+
